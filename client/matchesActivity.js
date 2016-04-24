c09a98219c06906b729c5aa77bb2ed5d0178cc26
Ethan Teague <ethan.teague@civicactions.com>
2016-04-23 22:37:24 -0400

    Fully functional4

--- client/matchesActivity.js
+++ client/matchesActivity.js
@@ -5,9 +5,9 @@ Template.matchesActivity.helpers({
       sort: {title: -1}, fields: {title: true, date: true, picked: true, actual: true}
     }).fetch().map(function (x) {
       cleanTime = new Date(x.date).toISOString().slice(0, 10);
-      return x.title + " | Date: " + cleanTime +  " | Actual: " + x.actual + " | Your Picks: " + x.picked ;
+      return {game: x.title, time: cleanTime, actual: x.actual, picked: x.picked};
     }), true);
-
+console.log(matches);
     return matches.slice(0, 5);
   }
 });
