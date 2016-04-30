ce49bfa8022d3f827efe882a0e734821e64e2070
Ethan Teague <ethan.teague@civicactions.com>
2016-04-29 23:06:06 -0400

    Updating to stay current.

--- client/matchesActivity.js
+++ client/matchesActivity.js
@@ -5,7 +5,20 @@ Template.matchesActivity.helpers({
       sort: {title: -1}, fields: {title: true, date: true, picked: true, actual: true}
     }).fetch().map(function (x) {
       cleanTime = new Date(x.date).toISOString().slice(0, 10);
-      return {game: x.title, time: cleanTime, actual: x.actual, picked: x.picked};
+
+      a = x.actual.split(/-| /);
+      p = x.picked.split(/-| /);
+      var matchClean = '';
+      for (i = 0; i < p.length; i++) {
+        if (p[i] == a[i]) {
+          matchClean += '<span class="badge match-highlight">' + p[i] + '</span>';
+        }
+        else {
+          matchClean += '<span class="badge">' + p[i] + '</span>';
+        }
+      }
+
+      return {game: x.title, time: cleanTime, actual: x.actual, picked: matchClean};
     }), true);
     return matches.slice(0, 5);
   }
