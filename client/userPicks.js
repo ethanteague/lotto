dad8534625a72e620b7e073d00a0b2de8c6e944b
Ethan Teague <ethan.teague@civicactions.com>
2016-04-30 11:18:53 -0400

    Updating to stay current.

--- client/userPicks.js
+++ client/userPicks.js
@@ -5,8 +5,15 @@ Template.userPicks.helpers({
     var groupedDates = _.groupBy(userPicks, 'title');
     cleanedItems = [];
     $.each(groupedDates, function () {
-      // grab the most recent num pick
-      cleanedItems.push(this.slice(-1).pop());
+      item = this.slice(-1).pop();
+      split = item.nums.split("-");
+      console.log(split);
+      var clean = '';
+      for (i = 0; i < split.length; i++) {
+        clean += '<span class="badge">' + split[i] + '</span>';
+      }
+      item.nums = clean;
+      cleanedItems.push(item);
     })
     return cleanedItems;
   }
