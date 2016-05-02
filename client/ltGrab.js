f4eb249c2c719ac2cc75b2424fc0e5f468fc832b
Ethan Teague <ethan.teague@civicactions.com>
2016-05-01 22:06:33 -0400

    Removing loader from a few templates

--- client/ltGrab.js
+++ client/ltGrab.js
@@ -24,7 +24,7 @@ Template.ltGrab.helpers({
 
       });
       callLotto();
-    }, 120 * 1000); // Poll every two minutes, set to 120 * 1000.
+    }, 20 * 1000); // Poll every two minutes, set to 120 * 1000.
     return theGames;
   }
 });
